1bba538f6d364bffb0a73217b14857720aab59fc
skskdmsdl <skskdmsdl@gmail.com>
2021-04-06 00:55:37 +0900

    antd 적용하여 로그인 폼 만들기

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import propTypes from 'prop-types';
 import Link from 'next/link';
-import { Menu } from 'antd';
-import 'antd/dist/antd.css';
+import { Menu, Input, Row, Col } from 'antd';
+import styled from 'styled-components';
+
+import UserProfile from '../components/UserProfile';
+import LoginForm from '../components/LoginForm';
+
+const SearchInput = styled(Input.Search)`
+    verticalAlign: middle;
+`;
 
 const AppLayout = ({ children }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
         <div>   
             <Menu mode="horizontal">
@@ -15,10 +23,24 @@ const AppLayout = ({ children }) => {
                     <Link href="/profile"><a>프로필</a></Link>
                 </Menu.Item>
                 <Menu.Item>
+                    <SearchInput />
+                </Menu.Item>
+                <Menu.Item>
                     <Link href="/signup"><a>회원가입</a></Link>
                 </Menu.Item>
             </Menu>
-            {children}
+            <Row gutter={8}>
+                <Col xs={24} md={6}>
+                    {isLoggedIn ? <UserProfile setIsLoggedIn={setIsLoggedIn} /> : <LoginForm setIsLoggedIn={setIsLoggedIn} />}
+                </Col>
+                <Col xs={24} md={12}>
+                    {children}
+                </Col>
+                <Col xs={24} md={6}>
+                    <a href="https://www.zerocho.com" target="_blank" rel="noreferrer noopener">Made by JEI</a>
+                </Col>
+            </Row>
+            
         </div>
     )
 };
---
+++ prepare/front/components/LoginForm.js
@@ -0,0 +1,64 @@
+import React, { useState, useCallback, useMemo } from 'react';
+import { Form, Input, Button } from 'antd';
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const ButtonWrapper = styled.div`
+    margin-top: 10px;
+`;
+
+const FormWrapper = styled(Form)`
+    padding: 10px;
+`;
+
+const LoginForm = ({ setIsLoggedIn }) => {
+    const [id, setId] = useState('');
+    const [password, setPassword] = useState('');
+
+    // 컴포넌트에 props로 넘겨주는 함수는 useCallback을 꼭 써라! 👉 그래야 최적화 됨
+    const onChangeId = useCallback((e) => {
+        setId(e.target.value);
+    }, []);
+
+    const onChangePassword = useCallback((e) => {
+        setPassword(e.target.value);
+    }, []);
+
+    // const style = useMemo(() => ({ marginTop: 10 }), []);
+    
+    const onSubmitFrom = useCallback(() => {
+        console.log(id, password);
+        setIsLoggedIn(true);
+    }, [id, password]);
+
+    return (
+        // onFinish는 자동으로 e.preventDefault()가 적용이 되어 있음(ant-design)
+        <FormWrapper onFinish={onSubmitFrom}>
+            <div>
+                <label htmlFor="user-id">아이디</label>
+                <br />
+                <Input name="user-id" value={id} onChange={onChangeId} required />
+            </div>
+            <div>
+                <label htmlFor="user-password">비밀번호</label>
+                <br />
+                <Input 
+                    name="user-password" 
+                    type="password" 
+                    value={password} 
+                    onChange={onChangePassword} 
+                    required 
+                />
+            </div>
+            <ButtonWrapper>
+                <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
+                <Link href="/singup"><a><Button>회원가입</Button></a></Link>
+            </ButtonWrapper>
+            <div>
+
+            </div>
+        </FormWrapper>
+    );
+}
+
+export default LoginForm;
