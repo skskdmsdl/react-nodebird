af23f25e662abbf288a51d07f2c042cc67d51aa0
skskdmsdl <skskdmsdl@gmail.com>
2021-04-05 20:53:26 +0900

    ant-design 추가

--- prepare/front/pages/signup.js
+++ prepare/front/pages/signup.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Head from 'next/head';
+
+import AppLayout from '../components/AppLayout';
 
 const Signup = () => {
-    return <div>회원가입 페이지</div>
+    return (
+        <>
+            <Head>
+                <title>회원가입 | NodeBird</title>
+            </Head>
+            <AppLayout>회원가입 페이지</AppLayout>
+        </>
+    );
 };
 
 export default Signup;
