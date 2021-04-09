571e3726d9cf0248cb676e406cdddb48cc284119
skskdmsdl <skskdmsdl@gmail.com>
2021-04-09 23:01:53 +0900

    redux 추가

--- prepare/front/pages/_app.js
+++ prepare/front/pages/_app.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import Head from 'next/head';
 import 'antd/dist/antd.css';
 
+import wrapper from '../store/configureStore';
+
 const NodeBird = ({ Component }) => {
     return (
         <>
@@ -18,4 +20,4 @@ const NodeBird = ({ Component }) => {
 NodeBird.propTypes = {
     Component: PropTypes.elementType.isRequired,
 }
-export default NodeBird;
+export default wrapper.withRedux.NodeBird;
