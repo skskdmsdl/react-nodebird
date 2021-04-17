b570eb67e8fbfdc6e23a1368a7139738b4db247b
skskdmsdl <skskdmsdl@gmail.com>
2021-04-17 22:43:41 +0900

    redux-thunk 추가

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,12 +1,20 @@
 import { createWrapper } from 'next-redux-wrapper';
 import { createStore, compose, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk';
 
 import reducer from '../reducers';
 
+// action을 실행하기 전에 console.log를 찍어주는 middleware
+// redux-devtools를 대체 할 수 있음
+const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
+    console.log(action);
+    return next(action);
+  };
+
 const configureStore = (context) => {
     console.log(context);
-    const middlewares = [];
+    const middlewares = [thunkMiddleware, loggerMiddleware];
     const enhancer = process.env.NODE_ENV === 'production'
     ? compose(applyMiddleware(...middlewares))
     : composeWithDevTools(
