4d2fb8952b3bf4b5bce1936d2364ffc60a28acd5
skskdmsdl <skskdmsdl@gmail.com>
2021-04-26 04:04:15 +0900

    MySQL과 시퀄라이즈 연결

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const postRouter = require('./routes/post');
 
 const app = express();
 
@@ -6,11 +7,11 @@ app.get('/', (req, res) => {
     res.send('hello express');
 });
 
-app.get('/api', (req, res) => {
-    res.send('hello ali');
+app.get('/', (req, res) => {
+    res.send('hello api');
 });
 
-app.get('/api/posts', (req, res) => {
+app.get('/posts', (req, res) => {
     res.json([
         { id: 1, content: 'hello1' },
         { id: 2, content: 'hello2' },
@@ -18,13 +19,7 @@ app.get('/api/posts', (req, res) => {
     ])
 });
 
-app.post('/api/post', (req, res) => {
-    res.json({ id: 1, content: 'hello1' });
-});
-
-app.delete('/api/post', (req, res) => {
-    res.json({ id: 1 });
-});
+app.use('/post', postRouter); // post가 prefix로 붙음
 
 app.listen(3000, () => {
     console.log('서버 실행 중');
