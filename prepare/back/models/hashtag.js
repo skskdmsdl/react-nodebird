bf0138b8267b7afd0f1e74b215ee236f59ab5f41
skskdmsdl <66931820+skskdmsdl@users.noreply.github.com>
2021-08-17 14:38:01 +0900

    Update hashtag.js

--- prepare/back/models/hashtag.js
+++ prepare/back/models/hashtag.js
@@ -4,7 +4,7 @@ const { Model } = DataTypes;
 module.exports = class Hashtag extends Model {
   static init(sequelize) {
     return super.init({
-      // id가 기본적으로 들어있다.
+      // id가 기본적으로 들어있음
       name: {
         type: DataTypes.STRING(20),
         allowNull: false,
